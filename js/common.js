65b8390b31b6f5f9d858b0765b0973861a72f79c
hraboviyvadim <hraboviyvadim@gmail.com>
2013-09-26 16:47:31 +0300

    sidebar__menu accordion added

--- js/common.js
+++ js/common.js
@@ -1,5 +1,28 @@
 $(document).ready(function() {
 
+	// accordeon sidebar__menu
+	$(".js-sidebar__sub-menu").hide();
+	$(".js-sidebar__menu").each(function(){
+		$(this).click(function(){
+			$(".js-sidebar__sub-menu").slideUp();
+			if($(this).hasClass('is-active')){
+				$(this).removeClass('is-active');
+				$(this).parent().find(".sidebar__sub-menu").slideUp();
+			}
+			else{
+				$(".js-sidebar__menu").removeClass('is-active');
+				$(this).addClass('is-active');
+				$(this).parent().find(".sidebar__sub-menu").slideDown();
+			}
+			
 
+
+
+
+			// $(this).toggleClass('is-active');
+			// $(".js-sidebar__sub-menu").hide();
+			// $(this).find(".sidebar__sub-menu").slideToggle();
+		});
+	});
 
 });
