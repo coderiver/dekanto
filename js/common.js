8e8c867428c28b4f52dc5de738ba19b6116c1603
hraboviyvadim <hraboviyvadim@gmail.com>
2013-09-26 17:19:00 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -1,27 +1,37 @@
 $(document).ready(function() {
 
-	// accordeon sidebar__menu
+	// accordion sidebar__menu
 	$(".js-sidebar__sub-menu").hide();
 	$(".js-sidebar__menu").each(function(){
 		$(this).click(function(){
 			$(".js-sidebar__sub-menu").slideUp();
+			$(".sidebar__l2-menu").slideUp();
 			if($(this).hasClass('is-active')){
 				$(this).removeClass('is-active');
 				$(this).parent().find(".sidebar__sub-menu").slideUp();
 			}
 			else{
 				$(".js-sidebar__menu").removeClass('is-active');
+				$(".js-sidebar__l2-menu").removeClass('is-active');
 				$(this).addClass('is-active');
 				$(this).parent().find(".sidebar__sub-menu").slideDown();
 			}
-			
+		});
+	});
 
-
-
-
-			// $(this).toggleClass('is-active');
-			// $(".js-sidebar__sub-menu").hide();
-			// $(this).find(".sidebar__sub-menu").slideToggle();
+	$(".sidebar__l2-menu").hide();
+	$(".js-sidebar__l2-menu").each(function(){
+		$(this).click(function(){
+			$(".sidebar__l2-menu").slideUp();
+			if($(this).hasClass('is-active')){
+				$(this).removeClass('is-active');
+				$(this).parent().find(".sidebar__l2-menu").slideUp();
+			}
+			else{
+				$(".js-sidebar__l2-menu").removeClass('is-active');
+				$(this).addClass('is-active');
+				$(this).parent().find(".sidebar__l2-menu").slideDown();
+			}
 		});
 	});
 
